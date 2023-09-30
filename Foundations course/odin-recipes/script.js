60f0f86c8ef8bafd12d0737ff57b308494025db5
M. Cesar Brancalhao <cesarbrancalhao@gmail.com>
2023-09-30 01:51:49 -0300

    Added onclick() redirect to divs

---
+++ Foundations course/odin-recipes/script.js
@@ -0,0 +1,12 @@
+function openNegroni() {
+    var negroniURL = './recipes/negroni.html';
+    window.open(negroniURL, '_blank');
+}
+function openMartini() {
+    var martiniURL = './recipes/martini.html';
+    window.open(martiniURL, '_blank');
+}
+function openMojito() {
+    var mojitoURL = './recipes/mojito.html';
+    window.open(mojitoURL, '_blank');
+}
